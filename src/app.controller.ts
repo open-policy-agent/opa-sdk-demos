3487cf8f0bd777495189313a8afd8bf553a3294d
Stephan Renatus <stephan@styra.com>
2024-03-13 12:21:07 +0100

    add passport/jwt (local) authentication
    Following mostly this guide: https://docs.nestjs.com/recipes/passport
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { Public } from './auth/decorators';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Public()
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Get('hello')
+  @Public()
+  getInfo() {
+    return { hello: 'world' };
   }
 }

0aef7a4aa09ba31b287ee1aee70ff70c8193ae89
Stephan Renatus <stephan@styra.com>
2024-03-13 11:22:54 +0100

    initial commit
    This cats service allows you to
    
        curl -v http://127.0.0.1:3000/cats -H "content-type: application/json" -d '{"name": "charly"}'
        curl -v 'http://127.0.0.1:3000/cats/charly'
        curl -v 'http://127.0.0.1:3000/cats'
    
    Started from github.com/nestjs/typescript-starter @7c21aea7e3b0767031c12345a701842ba483832e:
    
    Author: Kamil Myśliwiec <mail@kamilmysliwiec.com>
    Date:   Wed Feb 7 11:11:32 2024 +0100
    
        chore: update deps
    
    Then added cats stuff according to the Getting Started docs.

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':name')
+  async findByName(@Param('name') name: string): Promise<Cat> {
+    return this.catsService.findByName(name);
+  }
+}
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Cat } from './interfaces/cat.interface';
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Cat[] = [];
+
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+
+  findByName(name: string): Cat {
+    return this.cats.find((c: Cat) => c.name === name);
+  }
+}

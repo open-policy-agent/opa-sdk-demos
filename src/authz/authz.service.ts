8bab9ef6972c877b89928757a09479b3fd6bfe62
Stephan Renatus <stephan@styra.com>
2024-03-19 11:15:33 +0100

    opa-typescript: bump, adapt code
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- src/authz/authz.service.ts
+++ src/authz/authz.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Opa } from 'opa/sdk';
-import { authorizer } from 'opa/sdk/helpers';
+import { OPA } from 'opa/highlevel';
 
 @Injectable()
 export class AuthzService {
-  private opa: Opa;
+  private opa: OPA;
 
   constructor(private configService: ConfigService) {
-    this.opa = new Opa({ serverURL: this.configService.getOrThrow('OPA_URL') });
+    this.opa = new OPA(this.configService.getOrThrow('OPA_URL'));
   }
 
   async authorize(inp: Record<string, any>, path: string): Promise<boolean> {
-    return await authorizer<Record<string, any>, boolean>(this.opa, path)(inp);
+    return await this.opa.authorize<Record<string, any>, boolean>(path, inp);
   }
 }
